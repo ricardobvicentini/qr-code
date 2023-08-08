95e7a28715e9802eec282551d97ca871e69b57e2
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2023-08-08 11:57:56 -0300

    Fix reload()

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const myHeading = document.querySelector('.heading')
 const myText = document.querySelector('.text');
 const myImage = document.querySelector('.image-container');
-const myRefresh = `<p style="color: green;">Now click here to reload</p>`;
+const myRefresh = document.querySelector('.main-container');
+const myRefreshText = `<p style="color: green; cursor: pointer;">Now click here to reload</p>`;
+
 
 
 /* function clickHandler(event) {} */
@@ -10,13 +12,12 @@ const clickHandler = () => {
     myHeading.innerHTML = "DUDE!! Don't touch it, scan it!";
     myHeading.style.color = 'orangered';
     myText.innerHTML = "I knew you'd touch it. No worries! I just wanted to show a JS function!";
-    myText.insertAdjacentHTML('beforeend', myRefresh);
+    myText.insertAdjacentHTML('beforeend', myRefreshText);
+    const handleClick = () => {
+        window.location.reload();
+    };
+    
+    myText.addEventListener('click', handleClick);
 }
 
 myImage.addEventListener('click', clickHandler);
-
-const handleClick = () => {
-    window.location.reload();
-}
-
-myRefresh.addEventListener('click', handleClick);
