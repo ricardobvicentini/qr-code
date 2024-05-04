dfd1e2db56843454f2369ed581eeadc32f90259a
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-05-04 15:50:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-const myHeading = document.querySelector('.heading')
+const myHeading = document.querySelector('.heading');
 const myText = document.querySelector('.text');
 const myImage = document.querySelector('.image-container');
 const myRefresh = document.querySelector('.main-container');
 const myRefreshText = `<p style="color: green; cursor: pointer;">Now click here to reload</p>`;
 
-
-
 /* function clickHandler(event) {} */
 
 const clickHandler = () => {
-    myHeading.innerHTML = "DUDE!! Don't touch it, scan it!";
-    myHeading.style.color = 'orangered';
-    myText.innerHTML = "I knew you'd touch it. No worries! I just wanted to show a JS function!";
-    
-    setTimeout(function() {
-        myText.insertAdjacentHTML('beforeend', myRefreshText);
-    }, 4000);
+  myHeading.innerHTML = "DUDE!! Don't touch it, scan it!";
+  myHeading.style.color = 'orangered';
+  myText.innerHTML =
+    "I knew you'd touch it. No worries! I just wanted to show a JS function!";
 
-    const handleClick = () => {
-        window.location.reload();
-    };
-    
-    myText.addEventListener('click', handleClick);
-}
+  setTimeout(function () {
+    myText.insertAdjacentHTML('beforeend', myRefreshText);
+  }, 3000);
+
+  const handleClick = () => {
+    window.location.reload();
+  };
+
+  myText.addEventListener('click', handleClick);
+};
 
 myImage.addEventListener('click', clickHandler);
